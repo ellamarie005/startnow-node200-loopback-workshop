9fa0c50852a30f529b8914a35a68b1f8dab9a376
ellamarie005 <ellatolentino05@gmail.com>
2018-05-30 21:30:22 -0700

    initial and final project

---
+++ common/models/team.js
@@ -0,0 +1,5 @@
+'use strict';
+
+module.exports = function(Team) {
+  //team.disableRemoteMethod("delete", true);
+};
---
+++ server/datasources.local.js
@@ -0,0 +1,16 @@
+module.exports = {
+  "db": {
+    "name": "db",
+    "connector": "memory"
+  },
+  "MongoDb": {
+    "host": "",
+    "port": 0,
+    "url": "",
+    "database": "",
+    "password": "",
+    "name": "MongoDb",
+    "user": "",
+    "connector": "mongodb"
+  }
+}
